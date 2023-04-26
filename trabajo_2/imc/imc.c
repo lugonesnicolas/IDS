22be3fe32d1bff7bbdd3dfc78298e726f1c914f9
lugonesnicolas <lugones.nicolas@gmail.com>
2023-04-26 18:29:11 -0300

    Ejercicio IMC

--- trabajo_2/imc/imc.c
+++ trabajo_2/imc/imc.c
@@ -2,7 +2,35 @@
 
 int main()
 {
-    printf("Hola gil\nP\nU\nT\nO\n");
+    int peso = 0;
+    float altura = 0;
+    float imc = 0;
+
+    printf("Ingrese su peso en kg\n");
+    scanf("%d", &peso);
+    printf("Ingrese su altura en metros dividiendo el decimal por un '.'\n");
+    scanf("%f", &altura);
+
+    imc = peso / altura;
+
+    printf("Su IMC es:%f\n", imc);
+
+    if (imc < 18.5)
+    {
+        printf("Peso inferior de lo normal\n");
+    }
+    else if (imc <= 24.9)
+    {
+        printf("Peso normal\n");
+    }
+    else if (imc <= 29.9)
+    {
+        printf("Peso superior a lo normal\n");
+    }
+    else
+    {
+        printf("Obesidad\n");
+    }
 
     return (0);
 }
